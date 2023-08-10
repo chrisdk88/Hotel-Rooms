2a0ee4d6cc96383f237d3d5d112ea5fe8ebb9db9
Chris Sørensen <chso.skp@edu.mercantec.dk>
2023-08-10 12:02:40 +0200

    Add project files.

---
+++ Hotel Rooms/Controllers/RoomsControler.cs
@@ -0,0 +1,42 @@
+﻿using Hotel_Rooms.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace HotelH2.Controllers
+{
+    public class RoomsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Rooms> Rooms = new List<Rooms>();
+            string connectionString = "Data Source=PCVDATALAP117\\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT id, type, roomTemp, price, available, dateStart, dateEnd FROM [HotelRooms].[dbo].[Rooms]";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Rooms.Add(new Rooms
+                            {
+                                id = (int)reader["id"],
+                                type = reader["type"].ToString(),
+                                roomTemp = (int)reader["roomTemp"],
+                                price = (int)reader["price"],
+                                available = (bool)reader["available"],
+                                dateStart = (DateOnly)reader["dateStart"],
+                                dateEnd = (DateOnly)reader["dateEnd"],
+                            });
+                        }
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Hotel Rooms/Models/Rooms.cs
@@ -0,0 +1,14 @@
+﻿namespace Hotel_Rooms.Models
+{
+    public class Rooms
+    {
+        public int id { get; set; }
+        public string type { get; set; }
+        public int roomTemp { get; set; }
+        public int price { get; set; }
+        public bool available { get; set; }
+        public DateOnly dateStart { get; set; }
+        public DateOnly dateEnd { get; set; }
+
+    }
+}
