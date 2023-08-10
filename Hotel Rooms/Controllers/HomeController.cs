ef84fb1b27ec3acb5fee6135a535f01d05ea82c7
Chris Sørensen <chso.skp@edu.mercantec.dk>
2023-08-10 14:40:29 +0200

    Can print from database

--- Hotel Rooms/Controllers/HomeController.cs
+++ Hotel Rooms/Controllers/HomeController.cs
@@ -1,21 +1,20 @@
 ﻿using Hotel_Rooms.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 using System.Diagnostics;
 
-namespace Hotel_Rooms.Controllers
+namespace HotelH2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        public List<Rooms> RoomsList = new List<Rooms>();
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
-            return View();
+            ReadRoom(RoomsList);
+
+            return View(RoomsList);
         }
 
         public IActionResult Privacy()
@@ -27,6 +26,49 @@ namespace Hotel_Rooms.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public List<Rooms> ReadRoom(List<Rooms> RoomsList)
+        {
+            string connectionString = "Data Source=PCVDATALAP117\\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                try
+                {
+                    string query = "SELECT * FROM [HotelRooms].[dbo].[Rooms]";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Rooms tempList = new Rooms();
+
+                                tempList.id = (int)reader[0];
+                                tempList.type = (string)reader[1];
+                                tempList.roomTemp = (int)reader[2];
+                                tempList.price = (int)reader[3];
+                                tempList.available = (bool)reader[4];
+                                //tempList.dateStart = (DateOnly)reader[5];
+                                //tempList.dateEnd = (DateOnly)reader[6];
+
+                                RoomsList.Add(tempList);
+                            }
+
+                        }
+                    }
+                    connection.Close();
+                }
+                catch (SqlException e)
+                {
+
+                    throw e;
+                }
+            }
+            return RoomsList;
         }
     }
 }
--- Hotel Rooms/Controllers/RoomsController.cs
+++ Hotel Rooms/Controllers/RoomsController.cs
@@ -6,23 +6,24 @@ namespace HotelH2.Controllers
 {
     public class RoomsController : Controller
     {
+        //public List<Rooms> RoomsList = new List<Rooms>();
         public IActionResult Index()
         {
-            List<Rooms> Rooms = new List<Rooms>();
+            List<Rooms> myRooms = new List<Rooms>();
             string connectionString = "Data Source=PCVDATALAP117\\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
 
-                string query = "SELECT id, type, roomTemp, price, available, dateStart, dateEnd FROM [HotelRooms].[dbo].[Rooms]";
+                string query = "SELECT * FROM [HotelRooms].[dbo].[Rooms]";
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
-                            Rooms.Add(new Rooms
+                            myRooms.Add(new Rooms
                             {
                                 id = (int)reader["id"],
                                 type = reader["type"].ToString(),
@@ -36,7 +37,7 @@ namespace HotelH2.Controllers
                     }
                 }
             }
-            return View();
+            return View(myRooms);
         }
     }
 }
