180093cb554591f35db1eb45f989efa268deeea6
Chris Sørensen <chso.skp@edu.mercantec.dk>
2023-08-17 10:46:22 +0200

    Updated

--- Hotel Rooms/Controllers/HomeController.cs
+++ Hotel Rooms/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace HotelH2.Controllers
             return View();
         }
         [HttpPost]
-        public bool bookingUpdate(Rooms bookingUpdate)
+        public IActionResult bookingUpdate(Rooms bookingUpdate)
         {
             string query =  "UPDATE [HotelRooms].[dbo].[Rooms] " +
                             "SET type = @Type, price = @price, available = 1" +
@@ -35,28 +35,62 @@ namespace HotelH2.Controllers
                         int rowsAffected = cmd.ExecuteNonQuery();
 
 
-                        return rowsAffected > 0;
+                        return RedirectToAction("Status");
                     }
                 }
                 catch (SqlException ex)
                 {
                     // Handle exceptions here
-                    return false;
+                    return RedirectToAction("Status");
                 }
             }
         }
-        public IActionResult cancel()
+        public IActionResult cancel(int id)
         {
-            return View();
+            using (SqlConnection connection = new SqlConnection("Data Source=PCVDATALAP117\\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM [HotelRooms].[dbo].[Rooms] WHERE id = @ID";
+
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@ID", id);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                Rooms roomToUpdate = new Rooms();
+                                roomToUpdate.roomNumber = (int)reader[7];
+                                roomToUpdate.type = (string)reader[1];
+                                roomToUpdate.price = (int)reader[3];
+
+                                return View(roomToUpdate);
+                            }
+                            else
+                            {
+                                return NotFound();
+                            }
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    return View("Error");
+                }
+            }
         }
 
-        public bool bookingCancel(Rooms bookingCancel)
+        public IActionResult CancelBooking(Rooms bookingCancel)
         {
-            string query = "UPDATE [HotelRooms].[dbo].[Rooms]" +
-                            " SET available = 0" +
-                            " WHERE roomNumber = @roomNumber"; //bookingCancel.roomNumber;
 
-            using (SqlConnection connection = new SqlConnection("Data Source=PCVDATALAP117\\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+            string query = "UPDATE [HotelRooms].[dbo].[Rooms]" + 
+                            " SET available = 0, price = @price" +
+                            " WHERE roomNumber = @roomNumber";
+
+            using (SqlConnection connection = new SqlConnection("Data Source = PCVDATALAP117\\SQLEXPRESS; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"))
             {
                 try
                 {
@@ -64,38 +98,45 @@ namespace HotelH2.Controllers
 
                     using (SqlCommand cmd = new SqlCommand(query, connection))
                     {
-                        string setPrice = "0";
+                        int price = 0;
                         if (bookingCancel.type == "standard")
                         {
-                            setPrice = "600";
+                            price = 600;
                         }
                         else if (bookingCancel.type == "Premium")
                         {
-                            setPrice = "1200";
+                            price = 1200;
                         }
                         else if (bookingCancel.type == "Deluxe")
                         {
-                            setPrice = "2400";
+                            price = 2400;
                         }
+
                         cmd.Parameters.AddWithValue("@roomNumber", bookingCancel.roomNumber);
-                        cmd.Parameters.AddWithValue("@Type", bookingCancel.type);
-                        cmd.Parameters.AddWithValue(setPrice, bookingCancel.price);
-                        //cmd.Parameters.AddWithValue("@StartDate", bookingUpdate.dateStart);
-                        //cmd.Parameters.AddWithValue("@EndDate", bookingUpdate.dateEnd);
+                        cmd.Parameters.AddWithValue("@price", price);
 
                         int rowsAffected = cmd.ExecuteNonQuery();
 
-
-                        return rowsAffected > 0;
+                        if (rowsAffected > 0)
+                        {
+                            // Booking canceled successfully
+                            return RedirectToAction("Status");
+                        }
+                        else
+                        {
+                            // Booking cancellation failed
+                            return RedirectToAction("Status"); // Redirect to a failure page
+                        }
                     }
                 }
                 catch (SqlException ex)
                 {
                     // Handle exceptions here
-                    return false;
+                    return RedirectToAction("Status"); // Redirect to an error page
                 }
             }
         }
+
 
         public IActionResult Booking(int id)
         {
